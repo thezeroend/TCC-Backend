ed6df70c5f5fb42244b77252dda799800bcb8d3c
Matheus Soares <matheuslol150@gmail.com>
2020-09-07 22:00:57 -0300

    GitIgnore, Conexao MySQL
    GitIgnore, Conexao MySQL

--- api/controllers/faceController.js
+++ api/controllers/faceController.js
@@ -1,6 +1,13 @@
 'use strict';
 
+const mysql = require('mysql');
+
 exports.get_all = function(req, res) {
-	var return_teste = {'teste': '123'};
-	res.json(return_teste);
+	
+mysql.conexao.query('SELECT * FROM tb_usuarios', (err, rows) => {
+		if (err) throw err
+
+		//var return_teste = {'teste': '123'};
+		res.json(rows);
+	})
 }
--- server.js
+++ server.js
@@ -1,18 +1,39 @@
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
+  mysql = require('mysql'),
   bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+mysql.conexao = mysql.createConnection({
+	host: 'dbreconhecimento.cdqphiqba2oe.sa-east-1.rds.amazonaws.com',
+	port: '3306',
+	user: 'admin',
+	password: 'admin1234',
+	database: 'bd_facial'
+})
 
-var routes = require('./api/routes/faceRoutes'); //Importa as rotas
-routes(app);
+mysql.conexao.connect((err) => {
+    if (err) {
+        console.log('Erro ao conectar no banco de dados...', err)
+        return
+    }
+    console.log('Conexão com o banco realizada!')
 
-app.use(function(req, res) {
-  res.status(404).send({url: req.originalUrl + ' não encontrado'})
-});
+    app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.json());
 
-app.listen(port);
+	//Importa as rotas
+	var routes = require('./api/routes/faceRoutes');
+	routes(app);
 
-console.log('API server inciado na porta: ' + port);
+	//Retorno 404
+	app.use(function(req, res) {
+	  res.status(404).send({url: req.originalUrl + ' não encontrado'})
+	});
+
+	app.listen(port);
+
+	console.log('API server inciado na porta: ' + port);
+})
+
+
